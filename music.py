fd1a54418f517b700dd6afead16f8506cf9f7880
fearlessboss <fearlessaditya322@gmail.com>
2026-04-08 16:18:03 +0530

    Added all files

--- music.py
+++ music.py
@@ -91,7 +91,7 @@ load_local_env()
 REQUIRED_PACKAGES = {
     "pyrogram": "pyrogram>=2.0.106",
     "tgcrypto": "tgcrypto>=1.2.5",
-    "pytgcalls": "py-tgcalls>=2.1.0",
+    "pytgcalls": "py-tgcalls>=2.2.0",   # ← Bas yeh change kar do
     "yt_dlp": "yt-dlp>=2025.3.31",
 }
 
